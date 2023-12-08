56279d9f58f2ff9ad325fac20112a4af628eef09
Vũ Kiên Trường <91974329+KTruong9303@users.noreply.github.com>
2023-12-08 08:29:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import streamlit as st
+from PIL import Image
+import pickle as pkl
+import numpy as np
 
+input_ec = open('ec_vsfc.pkl', 'rb')
+encoder = pkl.load(input_ec)
+
+input_md = open('lrc_vsfc.pkl', 'rb')
+model = pkl.load(input_md)
+
+st.header('Write a feedback')
+txt = st.text_area('', '')
+
+if txt != '':
+  if st.button('Predict'):
+    feature_vector = encoder.transform([txt])
+    label = str((model.predict(feature_vector))[0])
+    st.header('Result')
+    st.text(class_list[label])
